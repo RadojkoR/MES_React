94dd5f05eb30fb864c0e5d87205622fac9812793
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-09-18 12:04:29 -0400

    added new icons

--- src/Components/HomeComponents/UslugeHome.tsx
+++ src/Components/HomeComponents/UslugeHome.tsx
@@ -1,4 +1,6 @@
-import {ReactComponent as Icon} from "../../Assets/icons/electricalInstallationIcon.svg";
+import {ReactComponent as IconElInstalacije} from "../../Assets/icons/electricalInstallationIcon.svg";
+import{ReactComponent as IconTehZastita} from "../../Assets/icons/emergency.svg";
+import{ReactComponent as IconSolarHouse} from "../../Assets/icons/solar-house.svg";
 
 export default function UslugeHome() {
     return (
@@ -6,7 +8,7 @@ export default function UslugeHome() {
         <div className="container">
           <section className="py-5 row justify-content-between">
             <div className="uslugeHomeCard col-4 d-flex flex-column justify-content-center align-items-center">
-              <Icon className="elInstalationIcon" />
+              <IconElInstalacije className="elInstalationIcon" />
               <article className="uslugeHomeCardBody">
                 <h4 className="text-center mb-4">Elektricne Instalacije</h4>
                 <p>
@@ -17,19 +19,23 @@ export default function UslugeHome() {
               </article>
             </div>
             <article className="uslugeHomeCard col-4 d-flex flex-column  justify-content-center align-items-center">
-              <Icon className="elInstalationIcon" />
+              <IconTehZastita className="elInstalationIcon" />
               <div className="uslugeHomeCardBody">
                 <h4 className="text-center mb-4">Tehnicka Zastita</h4>
-                <ul className="list-group">
+                <ul className="">
                   <li className="list-group">- Alarmni sistemi</li>
-                  <li className="list-group">- Video nadzor, kontrola pristupa</li>
+                  <li className="list-group">
+                    - Video nadzor, kontrola pristupa
+                  </li>
                   <li className="list-group">- Interfoni, video interfon</li>
-                  <li className="list-group">- Daljinsko otvaranje vrata i kapija</li>
+                  <li className="list-group">
+                    - Daljinsko otvaranje vrata i kapija
+                  </li>
                 </ul>
               </div>
             </article>
             <article className="uslugeHomeCard col-4 d-flex flex-column  justify-content-center align-items-center">
-              <Icon className="elInstalationIcon" />
+              <IconSolarHouse className="elInstalationIcon" />
               <div className="uslugeHomeCardBody">
                 <h4 className="text-center mb-4">Solarni Paneli</h4>
                 <p>
--- src/Components/Layout/Footer.tsx
+++ src/Components/Layout/Footer.tsx
@@ -8,16 +8,49 @@ export default function Footer() {
       <ul className="list-group list-group-horizontal fs-1 justify-content-center align-items-center">
         <li className="footerHrLine col-5"></li>
         <li className="footerSocIconLi list-group-item bg-transparent border-0 ">
-          <Link className="text-light" to="https://m.facebook.com/gradimir.jovicic.9" target="_blank"><FaFacebookF /></Link>
+          <Link
+            className="text-light"
+            to="https://m.facebook.com/gradimir.jovicic.9"
+            target="_blank"
+          >
+            <FaFacebookF />
+          </Link>
         </li>
         <li className="footerSocIconLi list-group-item bg-transparent border-0">
-          <Link className="text-light" to="https://www.instagram.com/" target="_blank"><FaInstagram /></Link>
+          <Link
+            className="text-light"
+            to="https://www.instagram.com/"
+            target="_blank"
+          >
+            <FaInstagram />
+          </Link>
         </li>
         <li className="footerHrLine col-5"></li>
       </ul>
+      <section className="row justify-content-between py-5">
+        <article className="col-6 d-flex flex-column justify-content-center align-items-center">
+          <h6 className="fs-3">Kontakt</h6>
+          <Link className="footerLink fs-5 mb-3" to="tel:+38132332216">Tel/Fax: +38132332216</Link>
+          <Link className="footerLink fs-5" to="tel:+381641837109">Tel/Fax: +381641837109</Link>
+        </article>
+        <article className="col-6 d-flex flex-column justify-content-center align-items-center">
+          <h6 className="fs-3">Adrese</h6>
+          <Link className="footerLink fs-5 mb-3" to="https://goo.gl/maps/hKGdCrnaC8M6sJ9e9" target="_blank">
+            Čačak : Jezdinsko Polje 11
+          </Link>
+          <Link className="footerLink fs-5" to="https://goo.gl/maps/7bi5bopffmo9T1Di7" target="_blank">
+            Beograd : Ivana Milutinovića 49, Borča
+          </Link>
+        </article>
+      </section>
       <section className="d-flex justify-content-center mt-5">
         <p>Copyright © 2024 All Rights Reserved M E S &emsp; | &emsp;</p>
-        <p>Powered by &nbsp;<Link className="powerdBy" to="https://radoyko.com" target="_blank">Radoyko.com</Link></p>
+        <p>
+          Powered by &nbsp;
+          <Link className="powerdBy" to="https://radoyko.com" target="_blank">
+            Radoyko.com
+          </Link>
+        </p>
       </section>
     </footer>
   );
