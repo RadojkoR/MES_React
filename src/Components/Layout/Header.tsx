07b618e35480266fcf29e5dd98dc2c6bec68036b
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-10-15 23:10:41 -0400

    ....

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -1,31 +1,19 @@
-// import videoFile from '../../Assets/videos/file.mp4';
-// import imgHeader from '../../Assets/imgHeader.jpg';
+
 
 interface HeaderProps {
   className: string;
   bgVideoClassName: string;
   headerContainerClass: string;
 }
-export default function Header({ className, bgVideoClassName, headerContainerClass }: HeaderProps) {
+export default function Header({ className, headerContainerClass }: HeaderProps) {
+  
 
   return (
     <header className={headerContainerClass}>
-      {/* <video
-        src={videoFile}
-        className={`${bgVideoClassName} position-absolute top-0 left-0`}
-        autoPlay
-        loop
-        muted
-      /> */}
-      <section className={`${className} position-absolute top-0 left-0`}>
-        {/* <div className="securityCameraImg position-absolute top-0"></div> */}
-        {/* <img src={imgHeader} alt="" /> */}
-        {/* <div className="headerLogo d-flex flex-column">
-           
-            <h1 className="m-0">MES</h1>
-            <p className="m-0 ps-2">Mega Elektro Security</p>
-          </div>  */}
-      </section>
+      <section
+        className={`${className} position-absolute top-0 left-0`}
+      ></section>
+     
     </header>
   );
 }
